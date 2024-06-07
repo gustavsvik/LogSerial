03dac5de834d9ef48e89a9cffaea34d6ab669095
Daniel Nilsson <daniel.nilsson@gustavsvik.eu>
2024-06-07 03:49:39 +0000

    Update LogSerial.h

--- include/LogSerial.h
+++ include/LogSerial.h
@@ -9,12 +9,12 @@ namespace Bus::SerialBus::Hard
   {
     public:
       #ifdef LOGLIMITLEVEL
-      /* LOGLIMITLEVEL is defined, i.e. Log class member functions will implement wrappers around Arduino library 
-         hardware serial communication library (Serial) functions and default to passing on their first argument to 
-         the appropriate Arduino Serial library function. Hardware serial port logging via an individual Log member 
-         function will then be effective if either no second argument (a_log_level) is passed, or it is passed and has 
-         an integer value greater than the default LOGLIMITLEVEL or whichever value of a_log_limit_level that the 
-         default is overridden by in the constructor (see implementation in LogSerial.cpp).*/
+      /* LOGLIMITLEVEL is defined in LogSerialSwitch.h, i.e. Log class member functions will implement wrappers around
+         Arduino library hardware serial communication library (Serial) functions and default to passing on their first
+         argument to the appropriate Arduino Serial library function. Hardware serial port logging via an individual
+         Log member function will then be effective if either no second argument (a_log_level) is passed, or it is
+         passed and has an integer value greater than the default LOGLIMITLEVEL or whichever value of a_log_limit_level
+         that the default is overridden by in the constructor (see implementation in LogSerial.cpp).*/
       Log(int a_baud_rate = 9600, int a_log_limit_level = LOGLIMITLEVEL);
       void write(int a_logged_int, int a_log_level = LOGLIMITLEVEL);
       void write(char a_logged_char, int a_log_level = LOGLIMITLEVEL);
@@ -28,9 +28,9 @@ namespace Bus::SerialBus::Hard
       void writeln(String a_logged_string, int a_log_level = LOGLIMITLEVEL);
       void writeln_hex(int a_logged_int, int a_log_level = LOGLIMITLEVEL);
       #else
-      /* LOGLIMITLEVEL is not defined, i.e. Log class members will compile to empty functions which will not perform 
-         any (useful) operations irrespective of argument(s), i.e. hardware serial port logging will effectively be 
-         disabled on a global level at compile time (see implementation in LogSerial.cpp). */
+      /* LOGLIMITLEVEL is not defined in LogSerialSwitch.h, i.e. Log class members will compile to empty functions
+         which will not perform any (useful) operations irrespective of argument(s), i.e. hardware serial port logging
+         will effectively be disabled on a global level at compile time (see implementation in LogSerial.cpp). */
       Log(...);
       void write(...);
       void write_hex(...);
